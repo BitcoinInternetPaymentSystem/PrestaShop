c964df99e778b706dc0f180fc5603882ad0af040
BitcoinInternetPaymentSystem <github@bips.me>
2013-07-30 16:21:50 +0200

    Update payment.php
    Checkout as guest.

--- upload/modules/BIPS/payment.php
+++ upload/modules/BIPS/payment.php
@@ -4,8 +4,8 @@ include(dirname(__FILE__).'/../../config/config.inc.php');
 include(dirname(__FILE__).'/../../header.php');
 include(dirname(__FILE__).'/BIPS.php');
 
-if (!$cookie->isLogged())
-    Tools::redirect('authentication.php?back=order.php');
+//if (!$cookie->isLogged())
+//    Tools::redirect('authentication.php?back=order.php');
    
 $BIPS = new BIPS();
 echo $BIPS->execPayment($cart);
