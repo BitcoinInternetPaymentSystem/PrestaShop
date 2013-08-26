89173750945cbd66fad131f1f08167faef3d10ea
BitcoinInternetPaymentSystem <github@bips.me>
2013-08-26 04:08:06 +0200

    Update BIPS.php

--- upload/modules/BIPS/BIPS.php
+++ upload/modules/BIPS/BIPS.php
@@ -205,14 +205,11 @@
 				$method = _PS_OS_PAYMENT_;
 			}
 
-			$this->validateOrder($cart->id, $method, $cart->getOrderTotal(true), $this->displayName, NULL, NULL, $currency->id);
-			$order = new Order($this->currentOrder);
-
 			$ch = curl_init();
 			curl_setopt_array($ch, array(
 			CURLOPT_URL => 'https://bips.me/api/v1/invoice',
 			CURLOPT_USERPWD => Configuration::get('BIPS_APIKEY'),
-			CURLOPT_POSTFIELDS => 'price=' . number_format($cart->getOrderTotal(true), 2, '.', '') . '&currency=' . $currency . '&item=Cart&custom=' . json_encode(array('order_id' => $order->id, 'cart_id' => $cart->id, 'returnurl' => rawurlencode((Configuration::get('PS_SSL_ENABLED') ? 'https://' : 'http://').htmlspecialchars($_SERVER['HTTP_HOST'], ENT_COMPAT, 'UTF-8').__PS_BASE_URI__.'order-confirmation.php?id_cart='.$cart->id), 'cancelurl' => rawurlencode((Configuration::get('PS_SSL_ENABLED') ? 'https://' : 'http://').htmlspecialchars($_SERVER['HTTP_HOST'], ENT_COMPAT, 'UTF-8').__PS_BASE_URI__.'order.php'))),
+			CURLOPT_POSTFIELDS => 'price=' . number_format($cart->getOrderTotal(true), 2, '.', '') . '&currency=' . $currency . '&item=Cart&custom=' . json_encode(array('cart_id' => $cart->id, 'returnurl' => rawurlencode((Configuration::get('PS_SSL_ENABLED') ? 'https://' : 'http://').htmlspecialchars($_SERVER['HTTP_HOST'], ENT_COMPAT, 'UTF-8').__PS_BASE_URI__.'order-confirmation.php?id_cart='.$cart->id), 'cancelurl' => rawurlencode((Configuration::get('PS_SSL_ENABLED') ? 'https://' : 'http://').htmlspecialchars($_SERVER['HTTP_HOST'], ENT_COMPAT, 'UTF-8').__PS_BASE_URI__.'order.php'))),
 			CURLOPT_RETURNTRANSFER => true,
 			CURLOPT_SSL_VERIFYHOST => 0,
 			CURLOPT_SSL_VERIFYPEER => 0,
